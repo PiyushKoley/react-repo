21e6fc740e7da004224d717b1bfcb494c53320e1
piyushkoley <piyushkoley@gmail.com>
2024-01-30 18:14:44 +0530

    done login form with react-hook-form

--- MegaBlogProject-withAppWrite/src/components/header/Header.jsx
+++ MegaBlogProject-withAppWrite/src/components/header/Header.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { LogoutBtn, Container, Logo } from "../index";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 
 function Header() {
     const authStatus = useSelector((state) => state.status);
@@ -36,9 +35,35 @@ function Header() {
     ];
 
     return (
-      <header>
+      <header className="py-3 shadow bg-gray-500">
         <Container >
-          
+          <nav className="flex">
+            <div className="mr-4">
+              <Link to="/">
+                <Logo width="70px"/>
+              </Link>
+            </div>
+
+            <ul className="flex ml-auto">
+              {navItems.map((item) => 
+                item.active ? (
+                  <li key={item.name}>
+                    <button
+                      onClick={() => navigate(item.slug)}
+                      className="inline-block px-6 py-2 duration-200 hover:bg-blue-200 rounded-full"
+                    >{item.name}</button>
+                  </li>
+                ) : null
+              )}
+
+              {authStatus && (
+                <li>
+                  <LogoutBtn />
+                </li>
+              )}
+            </ul>
+
+          </nav>
         </Container>
       </header>
     );
--- MegaBlogProject-withAppWrite/src/components/index.js
+++ MegaBlogProject-withAppWrite/src/components/index.js
@@ -3,6 +3,11 @@ import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import LogoutBtn from "./header/LogoutBtn";
 import Logo from "./Logo";
+import Button from "./Button";
+import Input from "./Input";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import Select from "./Select";
 
 
-export {Header, Footer, Container, Logo, LogoutBtn}
+export {Header, Footer, Container, Logo, LogoutBtn, Button, Input, Login, PostCard, Select }
